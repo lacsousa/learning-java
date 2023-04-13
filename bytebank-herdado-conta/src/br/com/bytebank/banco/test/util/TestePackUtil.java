bc7fc3689d977b546885fa613a5218103696bf6b
Luciano Cordeiro <lacs@petrobras.com.br>
2023-04-13 17:00:24 -0300

    ArrayList and Generics

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TestePackUtil.java
@@ -0,0 +1,37 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TestePackUtil {
+
+    public static void main(String[] args) {
+
+        ArrayList lista = new ArrayList<>();
+
+        Conta cc1 = new ContaCorrente(111, 345);
+        lista.add(cc1);
+
+        Conta cc2 = new ContaCorrente(42, 777);
+        lista.add(cc2);
+
+        Conta cc3 = new ContaCorrente(321, 555);
+        lista.add(cc3);
+
+        Conta cc4 = new ContaCorrente(678, 888);
+        lista.add(cc4);
+
+        for( int i=0; i< lista.size(); i++ ) {
+
+            System.out.println(lista.get(i));
+        }
+        
+        System.out.println("-------------");
+
+        for (Object c: lista) {
+            System.out.println(c);
+        }
+    }
+}
